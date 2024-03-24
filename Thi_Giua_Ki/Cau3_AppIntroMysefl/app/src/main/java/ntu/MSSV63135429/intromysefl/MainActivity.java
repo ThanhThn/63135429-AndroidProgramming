66e7487e91c3cfff920ae520415e0d04e3e86dc0
Lê Hoàng Quốc Thanh <lhqthanh1809@gmail.com>
2024-03-24 18:58:39 +0700

    hoàn thành sự kiện chuyển hướng sanng facebook

--- Thi_Giua_Ki/Cau3_AppIntroMysefl/app/src/main/java/ntu/MSSV63135429/intromysefl/MainActivity.java
+++ Thi_Giua_Ki/Cau3_AppIntroMysefl/app/src/main/java/ntu/MSSV63135429/intromysefl/MainActivity.java
@@ -2,6 +2,8 @@ package ntu.MSSV63135429.intromysefl;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
@@ -16,7 +18,8 @@ public class MainActivity extends AppCompatActivity {
     public void logout(View view){
         finish();
     }
-    public void rediretionFacebook(){
-        
+    public void redirectionFacebook(View view){
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/lehoangquocthan"));
+        startActivity(intent);
     }
 }
