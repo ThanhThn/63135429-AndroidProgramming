a17022c11cd25908c1af1961296c8dd84c4322a6
Lê Hoàng Quốc Thanh <lhqthanh1809@gmail.com>
2024-03-24 17:25:08 +0700

    code đăng nhập câu 3

--- Thi_Giua_Ki/Cau3_AppIntroMysefl/app/src/main/java/ntu/MSSV63135429/intromysefl/MainActivity.java
+++ Thi_Giua_Ki/Cau3_AppIntroMysefl/app/src/main/java/ntu/MSSV63135429/intromysefl/MainActivity.java
@@ -3,12 +3,29 @@ package ntu.MSSV63135429.intromysefl;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText txtUsername, txtPassword;
+    String username, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtPassword = findViewById(R.id.txtPassword);
+        txtUsername = findViewById(R.id.txtUserName);
+    }
+
+    public void login(){
+        username = txtUsername.getText().toString();
+        password = txtPassword.getText().toString();
+        if(username.equals("root") && password.equals("123456789")){
+
+        }else {
+            Toast.makeText(MainActivity.this, "Đăng nhập thất bại", Toast.LENGTH_SHORT).show();
+        }
     }
 }
